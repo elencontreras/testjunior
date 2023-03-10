c8e5855f9f9e749364ce2a4a4e756cb2787feb65
Elen <elencontreras6@gmail.com>
2023-03-10 15:51:10 -0500

    ajuste

--- testjunior/src/app/utils/custom-validations/expedition.validators.ts
+++ testjunior/src/app/utils/custom-validations/expedition.validators.ts
@@ -3,7 +3,8 @@ import * as moment from 'moment';
 
 export function expeditionValidator(control: AbstractControl) {
     let currentDate = new Date() 
-    if (moment(control.value).format('DD-MM-YYYY') >= moment(currentDate).format('DD-MM-YYYY') ) {
+    let val = new Date(control.value)
+    if (control.value >= currentDate ) {
         return { expeValid: true };
     }
   return null;
